25d631fabfd6d9a149d77c7a5b749e2c9ab883ad
codessbabe <oana.anghelachi@gmail.com>
2021-03-05 21:12:23 +0100

    Breathe animation with JS trigger and setTimeout, setInterval

---
+++ script.js
@@ -0,0 +1,24 @@
+// Select the DOM elements
+const container = document.getElementById("container")
+const text = document.getElementById("text")
+const totalTime = 7500
+const breatheTime = (totalTime / 5) * 2
+const holdTime = totalTime / 5
+
+breathAnimation()
+
+function breathAnimation() {
+  text.innerText = "Inspira! (Breathe In!)"
+  container.className = "container grow"
+
+  setTimeout(() => {
+    text.innerText = "Ține respirația! (Hold!)"
+
+    setTimeout(() => {
+      text.innerText = "Expira! (Breathe out!)"
+      container.className = "container shrink"
+    }, holdTime)
+  }, breatheTime)
+}
+
+setInterval(breathAnimation, totalTime)
